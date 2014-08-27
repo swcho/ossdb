e069f44c3156bf08cac69dda3fe3c3aa043dba28
sungwoo <swforgemaster@gmail.com>
2014-08-27 17:36:44 +0900

    sails generate api package sails generate api project sails generate api license sails generate api ossp

---
+++ api/controllers/LicenseController.js
@@ -0,0 +1,11 @@
+/**
+ * LicenseController
+ *
+ * @description :: Server-side logic for managing licenses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/OsspController.js
@@ -0,0 +1,11 @@
+/**
+ * OsspController
+ *
+ * @description :: Server-side logic for managing ossps
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
