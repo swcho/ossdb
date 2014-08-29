f3dbbe15278ccf2fc65a0023bf44cfce1f11b8c4
sungwoo <swforgemaster@gmail.com>
2014-08-29 11:11:24 +0900

    page no decrease 1 for proper skip

--- api/controllers/OsspController.js
+++ api/controllers/OsspController.js
@@ -22,7 +22,7 @@ module.exports = {
         var limit = parseInt(req.param('limit'), 10) || 10;
         var sort = req.param('sort');
         console.log([no, limit, sort].join(', '));
-        Ossp.find({}).skip(no * limit).limit(limit).sort(sort).exec(function findCB(err, found) {
+        Ossp.find({}).skip((no - 1) * limit).limit(limit).sort(sort).exec(function findCB(err, found) {
             Ossp.count().exec(function (err, count) {
                 res.json({
                     count: count,
