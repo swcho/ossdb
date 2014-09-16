8f217cbf8c7d306b66d8a71479312618f97ae7d2
sungwoo <swforgemaster@gmail.com>
2014-09-16 15:19:10 +0900

    sails generate controller Auth

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,11 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing Auths
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
