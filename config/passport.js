60729db82af8b99c8b23db0af505e2877ab2e303
sungwoo <swforgemaster@gmail.com>
2014-09-16 13:14:13 +0900

    Install passport typescript definition

---
+++ config/passport.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sungwoo on 14. 9. 16.
+ */
+
+var passport = require('passport');
+var LocalStorage = require('passport-local').Strategy;
+var bycrypt = require('bycrypt');
+
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -8,3 +8,4 @@
 /// <reference path="mocha/mocha.d.ts" />
 /// <reference path="supertest/supertest.d.ts" />
 /// <reference path="superagent/superagent.d.ts" />
+/// <reference path="passport/passport.d.ts" />
